8814458bd3f258cd5f52d5b7b95af43388cce14e
YJU <kangyoungjun@g.yju.ac.kr>
2026-04-02 12:24:05 +0900

    JPARepository 실습 및 테스트 코드 추가

--- src/main/java/me/dmddo/springdeveloper/Member.java
+++ src/main/java/me/dmddo/springdeveloper/Member.java
@@ -19,8 +19,11 @@ public class Member {
     @Column(name = "name", nullable = false)
     private String name;
 
+    public void changeName(String name) {
+        this.name = name;
+    }
+
     public Member(String name){
         this.name=name;
-
     }
 }
